45536e3fb2746ed60e1337ba4337b095a017b1c0
Amnok <ankitsingh80741@gmail.com>
2022-01-14 11:38:48 +0530

    UI level fixes

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import { addBug, bugResolved, getBugs, bugAssignedToUser } from './store/bugs';
 import { addProject } from './store/projects';
 
 export default function CustomRedux() {
+  store.dispatch(addBug({ description: 'Bug 1' }));
+  store.dispatch(addBug({ description: 'Bug 2' }));
+  store.dispatch(addBug({ description: 'Bug 3' }));
+  store.dispatch(bugAssignedToUser({ id: 2, userId: 2 }));
+  store.dispatch(bugResolved({ id: 2 }));
 
-  store.dispatch(addBug({description: 'Bug 1' }));
-  store.dispatch(addBug({description: 'Bug 2' }));
-  store.dispatch(addBug({description: 'Bug 3' }));
-  store.dispatch(bugAssignedToUser({id: 1, userId: 2}));
-  store.dispatch(bugResolved({id: 2 }));
-
-store.dispatch(addProject({name: 'first projects'}))
+  store.dispatch(addProject({ name: 'first projects' }));
   console.log(store.getState());
   return <div>Hello</div>;
 }
